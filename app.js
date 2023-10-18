853f531193b4e36d4e5b6a741f96a491cc822f9f
Albina Kalimulina <albinashamig@yandex.ru>
2023-10-19 04:12:16 +0500

    диплом

---
+++ app.js
@@ -0,0 +1,73 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const NotFoundError = require('./errors/not-found-err');
+
+const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.use(cors());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(helmet());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+});
+
+app.use(requestLogger); // подключаем логгер запросов
+
+app.use(limiter);
+
+// app.get('/crash-test', () => {
+//   setTimeout(() => {
+//     throw new Error('Сервер сейчас упадёт');
+//   }, 0);
+// });
+
+app.use('/', require('./routes/signup'));
+app.use('/', require('./routes/signin'));
+
+app.use(auth);
+
+app.use('/users', require('./routes/users'));
+app.use('/movies', require('./routes/movies'));
+
+app.use('*', (req, res, next) => {
+  next(new NotFoundError('Страницы не существует'));
+});
+
+app.use(errorLogger);
+
+app.use(errors());
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+  next();
+});
+
+app.listen(PORT, () => {
+  // console.log('успешное подключение');
+});
